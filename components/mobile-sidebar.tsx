69c594490a1262be6692cc400c484712482124e1
Jerome Ghartey <gharteyjerome@gmail.com>
2023-08-02 18:03:00 +0100

    fixed errors

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import { Menu } from "lucide-react";
 
-import { Menu } from "lucide-react"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sidebar } from "@/components/Sidebar";
 
-import {
-    Sheet, SheetContent, SheetTrigger
-} from "@/components/ui/sheet"
-import { Sidebar } from './Sidebar'
-
-export const MobileSidebar = () => {
-    return (
-        <Sheet>
-            <SheetTrigger className='md:hidden pr-4'>
-                <Menu />
-            </SheetTrigger>
-            <SheetContent side="left" className='p-0 bg-secondary pt-10 w-32'>
-                <Sidebar />
-            </SheetContent>
-        </Sheet>
-    )
-}
+export const MobileSidebar = ({
+  isPro
+}: {
+  isPro: boolean;
+}) => {
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
+        <Sidebar isPro={isPro} />
+      </SheetContent>
+    </Sheet>
+  );
+};
